c2a5974f2956719c39997eede36b9d7621ead8e1
Ayush <ayushrock1010d.123@gmail.com>
2023-09-24 00:10:07 +0530

    logout session done

---
+++ database/logout.php
@@ -0,0 +1,9 @@
+<?php
+   //starts the session 
+   session_start();
+
+   //unsets the session
+   session_unset();
+
+   session_destroy();//destroys it 
+?>
--- login.php
+++ login.php
@@ -1,48 +1,54 @@
 <?php
-     //start of thr session 
-	 session_start();
+//start of thr session 
+session_start();
+  if(isset($_SESSION['user'])) header('location :dashboard.php');
 
-      $error_message ="";
+$error_message = "";
 
-     if($_POST){
+if ($_POST) {
 
-		include('database/connection.php');
-		$username =$_POST['username'];
-		$password =$_POST['password'];
-		 
-		$query = 'SELECT * FROM users WHERE users.email="'.$username . '" AND users.password="'.$password.'"';
-		$stmt = $conn->prepare($query);
-		$stmt->execute();
-		if($stmt->rowCount()>0){
-			$stmt->setFetchMode(PDO::FETCH_ASSOC);
-			$user =$stmt->fetchAll()[0];
-			$_SESSION['user']=$user;
-			
-			header('Location: dashboard.php');
-		}else $error_message ='Please enter the username and password correctly';
-	 }
+	include('database/connection.php');
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+
+	$query = 'SELECT * FROM users WHERE users.email="' . $username . '" AND users.password="' . $password . '"';
+	$stmt = $conn->prepare($query);
+	$stmt->execute();
+	if ($stmt->rowCount() > 0) {
+		$stmt->setFetchMode(PDO::FETCH_ASSOC);
+		$user = $stmt->fetchAll()[0];
+		$_SESSION['user'] = $user;
+
+		header('Location: dashboard.php');
+	} else
+		$error_message = 'Please enter the username and password correctly';
+}
 ?>
 
 
 <!DOCTYPE html>
 <html>
+
 <head>
 	<title>IMS Login - Inventory Management System</title>
 	<link rel="stylesheet" type="text/css" href="css/login.css">
 </head>
+
 <body id="loginBody">
-	<?php if(!empty($error_message)){?>
-	<div id="errorMessage">
-		<p><strong>Error :</strong> <?= $error_message?> </p>
-    </div>
-     <?php }?>
+	<?php if (!empty($error_message)) { ?>
+		<div id="errorMessage">
+			<p><strong>Error :</strong>
+				<?= $error_message ?>
+			</p>
+		</div>
+	<?php } ?>
 	<div class="container">
 		<div class="loginHeader">
 			<h1>IMS</h1>
 			<p>Inventory Management System</p>
 		</div>
 		<div class="loginBody">
-			<form action ="login.php" method ="POST">
+			<form action="login.php" method="POST">
 				<div class="loginInputsContainer">
 					<label for="">Username</label>
 					<input placeholder="username" name="username" type="text" />
@@ -58,4 +64,5 @@
 		</div>
 	</div>
 </body>
-</html> 
+
+</html>
