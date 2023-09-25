1349a9ba3d4096ff4470eaad15ba4f554aadd950
harsh252203 <hs41061@gmail.com>
2023-09-26 00:42:07 +0530

    harsh

--- users-add.php
+++ users-add.php
@@ -3,6 +3,7 @@ session_start();
 if(!isset($_SESSION['user'])) header('location :login.php');
 $_SESSION['table'] = 'users';
 $user = $_SESSION['user'];
+$users = include('database/show-users.php');
 ?>
 
 <!DOCTYPE html>
@@ -21,48 +22,94 @@ $user = $_SESSION['user'];
         
         <div class="dasboard_content_container" id="dasboard_content_container">
         <?php include('partials/app-topnav.php') ?>
-            <div class="dashboard_content">
-                <div class="dashboard_content_main">
-                    <div id="userAddFormContainer">
+            <div class="dashboard_content"> 
+            <div class="dashboard_content_main">
+            <div class="row">
+              <div class="column column-5">
+               <h1 class="section_header"><i class="fa fa-plus"></i> Create User</h1>
+             
+             <div id="userAddFormContainer">
 
-                    <form action="database/add.php" method="POST" class="appForm">
-                    <div class="appFormInputContainer">
-                        <label for="first_name">First Name</label>
-                        <input type="text" class="appFormInput" id="first_name" name="first_name"/>
-                </div>
-                <div class="appFormInputContainer">
-                <label for="last_name">Last Name</label>
-                        <input type="text" class="appFormInput" id="last_name" name="last_name"/> 
-            </div>
-            <div class="appFormInputContainer">
-                <label for="email">email</label>
-                        <input type="text" class="appFormInput" id="email" name="email"/> 
-            </div>
-            <div class="appFormInputContainer">
-                <label for="password">Password</label>
-                        <input type="password" class="appFormInput" id="password" name="password"/> 
-            </div>
-          
-            <button type="submit" class="appBtn"><i class="fa fa-plus"></i>Add User</button>
-        
-    </form>
-         <?php 
-         
-            if(isset($_SESSION['response'])) {
-                $response_message = $_SESSION['response']['message'];
-                $is_success = $_SESSION['response']['success'];
-                ?>
-            <div class="responseMessage">
-             <p class =" responseMessage <?= $is_success ? 'responseMessage_success' : 'responseMessage_error' ?>">
-             <?= $response_message ?>
-                 </p>
-         </div>
-            <?php unset($_SESSION['response']); } ?>
+          <form action="database/add.php" method="POST" class="appForm">
+           <div class="appFormInputContainer">
+    <label for="first_name">First Name</label>
+    <input type="text" class="appFormInput" id="first_name" name="first_name"/>
+</div>
+<div class="appFormInputContainer">
+<label for="last_name">Last Name</label>
+    <input type="text" class="appFormInput" id="last_name" name="last_name"/> 
+</div>
+<div class="appFormInputContainer">
+<label for="email">email</label>
+    <input type="text" class="appFormInput" id="email" name="email"/> 
+</div>
+<div class="appFormInputContainer">
+<label for="password">Password</label>
+    <input type="password" class="appFormInput" id="password" name="password"/> 
+</div>
+
+<button type="submit" class="appBtn"><i class="fa fa-plus"></i>Add User</button>
+
+</form>
+<?php 
+
+if(isset($_SESSION['response'])) {
+$response_message = $_SESSION['response']['message'];
+$is_success = $_SESSION['response']['success'];
+?>
+<div class="responseMessage">
+<p class =" responseMessage <?= $is_success ? 'responseMessage_success' : 'responseMessage_error' ?>">
+<?= $response_message ?>
+</p>
+</div>
+<?php unset($_SESSION['response']); } ?>
+</div>
+   </div>  
+ 
+     
+             
+                         <div class="column column-7">
+                          <h1 class="section_header"><i class="fa fa-list"></i> List of Users</h1>
+                  <div class="section_content">
+                   <div class="users">
+                          <table>
+                          <thead>
+                          <tr>
+                           <th> # </th>
+                            <th> First Name </th>
+                            <th> Last Name </th>
+                            <th> email </th>
+                            <th> Created At</th>
+                            <th> Updated At</th>
+                           </tr>
+                          </thead>
+
+                           <tbody>
+                            <tr> 
+                                <td>1</td>
+                                <td>Harsh</td>
+                                <td>Sharma</td>
+                                <td>hs41061@gmail.com</td>
+                                <td>March 27, 2023 @ 12:30pm</td>
+                                <td>March 29,2023 @ 1:30pm</td>
+                            </tr>
+                           </tbody>
+
+                
+                          </table>
+                   </div>
+                  </div>
+
+             
+            
                </div>
+               
+            </div>  
           </div>
       </div>
    </div>
 </div>
+
 
     <script src="js/script.js"> </script>
 </body>
